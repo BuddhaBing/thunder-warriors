34ac5f40616f41eb344ebc09ee486cfd9d6ae2b6
James Turner <mail.jt@gmail.com>
2017-03-15 16:35:53 +0000

    teststeststests

---
+++ Assets/80std/Script/EndPointMotor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPointMotor : MonoBehaviour {
+	private EndPointLogic EndPointLogic = new EndPointLogic();
+
+	void OnEnable(){
+		EndPointLogic.MyMotor = this;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		EndPointLogic.EnemyLeaked (collider.gameObject);
+	}
+
+	public void DestroyItem(GameObject obj){
+		GameObject.Destroy (obj);
+	}
+}
+
+public class EndPointLogic{
+	public EndPointMotor MyMotor;
+
+	public void EnemyLeaked(GameObject Enemy){
+		//if(Array.IndexOf (MyEnemyManager.All (), Enemy) == -1){return;}
+		//call player to remove n lives
+		GameObject.Destroy(Enemy);
+	}
+}
--- Assets/Editor/EndPointTest.cs
+++ Assets/Editor/EndPointTest.cs
@@ -1,18 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using NSubstitute;
 
 public class EndPointTest {
 
 	[Test]
 	public void EditorTest() {
 		//Arrange
-		var Enemy = new GameObject();
-		var EndPointLogic = new EndPointLogic();
-		//Act
-		EndPointLogic.EnemyLeaked(Enemy);
+		var enemy = new GameObject();
+		var endPointLogic = new EndPointLogic();
+		var endPointMotor = GetMotorMock();
+		endPointLogic.MyMotor = endPointMotor;
 
+		//Act
+		//endPointLogic.EnemyLeaked(enemy);
 		//Assert
-		Assert.IsFalse(Enemy);
+		endPointMotor.Received().DestroyItem(enemy);
+	}
+
+	public EndPointMotor GetMotorMock(){
+		EndPointMotor EPM = Substitute.For<EndPointMotor>();
+		return EPM;
 	}
 }
